b5b5537367bf59d9a38f51283e0548f6e0291aaf
bemybunny <bemybunny123@gmail.com>
2024-06-14 12:25:19 +0530

    add

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://form-registration-five.vercel.app'
+}));
 require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE)
